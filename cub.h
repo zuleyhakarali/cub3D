d80ad4f31982d696b4ffe2e2058d1cb2e559d3ca
Züleyha Karali <zuleyha.karali04@gmail.com>
2026-08-11 17:08:28 +0300

    structs

--- cub.h
+++ cub.h
@@ -10,23 +10,50 @@
 #include <math.h>      // Matematik kütüphanesi fonksiyonları (-lm)
 #include <mlx.h>       // MiniLibX grafik kütüphanesi
 
+typedef enum e_tex // Doku dizisinin indeks isimleri
+{
+    NO_t = 0,
+    SO_t = 1,
+    WE_t = 2,
+    EA_t = 3
+}   t_tex;
+
 typedef struct s_cub
 {
-		char    **cub;
-		int     x;// ya da double
-		int     y;
-		int		m_height;
-		int		m_width;
-		
+		char    **cub; //harita
+		double	x; //px pozisyon
+		double	y; //py pozisyon
+		double	x_dir; //px yönü
+		double	y_dir; //py yönü
+		double	x_plane; // kamera düzlemi x
+		double	y_plane; // kamera düzlemi y
+		int		height; //yükseklik
+		int		width; //genişlik
+		int		floor; //taban renk
+		int		ceil; ///tavan renk
+		char	*no;
+		char	*so;
+		char	*we;
+		char	*ea;
 }       t_cub;
+
+typedef struct s_img
+{
+	void	*img;
+	char	*pix_ptr;
+	int		bits_per_pix;
+	int		line_l;
+	int		endian;
+	int		wid;
+	int		heig;
+}	t_img;
 
 typedef struct s_mlx
 {
 		void	*mlx;
         void	*window;
-        void	*iwall;
-        void	*ifloor;
-		void	*iground;
+        t_img	screen_b;
+		t_img	textures[4];
 }		t_mlx;
 
 int main(int ac, char **av);
